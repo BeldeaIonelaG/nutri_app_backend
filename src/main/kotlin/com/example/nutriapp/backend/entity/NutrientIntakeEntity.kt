c5dd9487b05b8955847fdb9ce729627b9a1a2abf
Georgiana Beldea <gbeldea@gmail.com>
2026-05-05 22:17:16 +0300

    change intake_nutrient1

--- src/main/kotlin/com/example/nutriapp/backend/entity/NutrientIntakeEntity.kt
+++ src/main/kotlin/com/example/nutriapp/backend/entity/NutrientIntakeEntity.kt
@@ -1,6 +1,8 @@
 package com.example.nutriapp.backend.entity
 
 import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+import jakarta.persistence.EmbeddedId
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -8,29 +10,27 @@ import jakarta.persistence.Id
 import jakarta.persistence.IdClass
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import jakarta.persistence.MapsId
 import jakarta.persistence.Table
 import java.io.Serializable
 
-data class NutrientIntakeId(
+@Embeddable
+data class NutrientIntakeKey(
     val intakeId: Int = 0,
     val nutrientId: Int = 0
 ) : Serializable
 
 @Entity
 @Table(name = "nutrient_intake")
-@IdClass(NutrientIntakeId::class)
-class NutrientIntakeEntity(
+data class NutrientIntakeEntity(
 
+    @EmbeddedId
+    val id: NutrientIntakeKey,
 
-    @Column(name = "intake_id")
-    var intakeId: Int,
-
-    @Column(name = "nutrient_id")
-    var nutrientId: Int,
-
-    var quantity: Double,
+    val quantity: Double,
 
     @ManyToOne
-    @JoinColumn(name = "intake_id", insertable = false, updatable = false)
-    var intake: IntakeEntity? = null
+    @MapsId("intakeId")
+    @JoinColumn(name = "intake_id")
+    val intake: IntakeEntity
 )
