99e19358cac8d2e862c6e834555d85af7ceadc83
Georgiana Beldea <gbeldea@gmail.com>
2026-05-05 22:41:11 +0300

    change intake_nutrient3

--- src/main/kotlin/com/example/nutriapp/backend/repository/NutrientIntakeRepository.kt
+++ src/main/kotlin/com/example/nutriapp/backend/repository/NutrientIntakeRepository.kt
@@ -1,8 +1,8 @@
 package com.example.nutriapp.backend.repository
 
 import com.example.nutriapp.backend.entity.NutrientIntakeEntity
-import com.example.nutriapp.backend.entity.NutrientIntakeId
+import com.example.nutriapp.backend.entity.NutrientIntakeKey
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface NutrientIntakeRepository :
-    JpaRepository<NutrientIntakeEntity, NutrientIntakeId>
+    JpaRepository<NutrientIntakeEntity, NutrientIntakeKey>
