ea1603c8c8834d73519a953a3072f7a3f150e8e1
Georgiana Beldea <gbeldea@gmail.com>
2026-05-16 23:00:19 +0300

    get users2

--- src/main/kotlin/com/example/nutriapp/backend/service/JWTService.kt
+++ src/main/kotlin/com/example/nutriapp/backend/service/JWTService.kt
@@ -161,7 +161,7 @@ class SecurityConfig(
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .authorizeHttpRequests {
                 it
-                    .requestMatchers("/auth/**","users/**").permitAll()
+                    .requestMatchers("/auth/**").permitAll()
                     .requestMatchers("/**").hasRole("USER")
                     .anyRequest().authenticated()
             }
